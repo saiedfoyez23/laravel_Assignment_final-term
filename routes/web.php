4605fefefeefbb6d66854fd3bc75a244bb0c29d9
saiedfoyez23 <saiedfoyez@gmail.com>
2020-03-30 23:00:00 +0600

    Basic route operation

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home',function(){
+    echo " Hi! saied ahammed Foyez this is your home";
+});
+
+Route::get('/news',function(){
+    echo "This is our news protal";
+});
+
+Route::get('/contact',function(){
+    echo "view all contact in our protal";
+});
+
+Route::get('/order',function(){
+    echo "<h1>Place some order in here </h1>";
+});
+//match('get','post')
